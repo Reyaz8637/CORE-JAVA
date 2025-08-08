e7ee6dcdd6c3790c3cc82f4c58b9ed3ca0d4bf29
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 16:41:35 +0530

    Add files via upload

---
+++ BorderLayoutdemo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package borderlayoutdemo;
+import java.awt.*;
+
+class MyFrame extends Frame{
+    Button b1,b2,b3,b4,b5,b6;
+   public MyFrame(){
+        super("BorderLayout demo");
+        
+        b1=new Button("one");
+        b2=new Button("two");
+        b3=new Button("three");
+        b4=new Button("four");
+        b5=new Button("five");
+        b6=new Button("six");
+        
+        add(b1,BorderLayout.NORTH);
+//        add(b2,BorderLayout.EAST);
+        add(b3,BorderLayout.SOUTH);
+        add(b4,BorderLayout.WEST);
+        add(b5,BorderLayout.CENTER);
+        
+        Panel p=new Panel();
+        p.setLayout(new GridLayout(3,1));
+        p.add(new Button("Mon"));
+        p.add(new Button("Tue"));
+        p.add(new Button("Wed"));
+        add(p,BorderLayout.EAST);
+        
+       
+    }
+}
+public class BorderLayoutdemo {
+
+    public static void main(String[] args) {
+       MyFrame f=new MyFrame();
+       f.setSize(500,500);
+       f.setVisible(true);
+    }
+    
+}
+
